3ce42d779f197745cfd3eb73c95a7599d3bb75c5
muzammil-x <m.muzammilx2@gmail.com>
2023-08-15 22:00:57 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Food from "./components/Food";
+import Category from "./components/Category";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
+      <Food />
+      <Category />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,27 @@
+import React from "react";
+
 const Hero = () => {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1640px] mx-auto p-4">
+      <div className="max-h-[500] relative">
         {/* overlay */}
-        <div>
-          <h1>
-            The <span>Best</span>
+        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            <b>
+              The <span className="text-orange-500">Best</span>
+            </b>
           </h1>
-          <h1>
-            Foods <span>Delivered</span>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            <b>
+              <span className="text-orange-500">Foods</span> Delivered
+            </b>
           </h1>
         </div>
+        <img
+          src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="/"
+          className="w-full max-h-[500px] object-cover"
+        />
       </div>
     </div>
   );
