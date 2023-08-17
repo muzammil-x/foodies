2e97d92c6143503d480830108900985ca8c1ab91
muzammil-x <m.muzammilx2@gmail.com>
2023-08-17 15:47:54 +0500

    final updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="max-w-[1640px] mx-auto-p-4 py-12 grid md:grid-cols-3 gap-6">
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
       {/* card */}
       <div className="rounded-xl relative">
         {/* overlay */}
